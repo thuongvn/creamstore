63d78cba0393a97d1522f92604dbd6effc4c81a7
Thuong Vu <ngocthuongfcvn@gmail.com>
2020-01-02 10:08:12 +0700

    commit entittes

--- back-end/CreamStore/src/main/java/com/example/demo/entity/Transaction.java
+++ back-end/CreamStore/src/main/java/com/example/demo/entity/Transaction.java
@@ -12,9 +12,9 @@ public class Transaction {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private int id;
-    @ManyToOne
-    @JoinColumn(name = "user")
-    private User user;
+//    @ManyToOne
+//    @JoinColumn(name = "user")
+//    private User user;
 
     @Column(name="create_at")
     private Date create_at;
