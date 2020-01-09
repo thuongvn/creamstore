7cce41b62a50f993e220dc8418e71f7092bb32b7
Thuong Vu <ngocthuongfcvn@gmail.com>
2020-01-09 16:55:38 +0700

    fix cors

--- back-end/CreamStore/src/main/java/com/example/demo/sercurity/SecurityConfig.java
+++ back-end/CreamStore/src/main/java/com/example/demo/sercurity/SecurityConfig.java
@@ -20,7 +20,21 @@ import org.springframework.web.filter.CorsFilter;
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     private RestAuthenticationEntryPoint restAuthenticationEntryPoint;
-
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("*");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("OPTIONS");
+        config.addAllowedMethod("GET");
+        config.addAllowedMethod("POST");
+        config.addAllowedMethod("PUT");
+        config.addAllowedMethod("DELETE");
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
     @Bean
     public FilterRegistrationBean filterRegistrationBean(){
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
